90ea7884a490071f8ea3d30eb2664232df76df1b
ART0 <18333981+0Art0@users.noreply.github.com>
2023-11-16 15:19:25 +0000

    Script for Json output of `DeclDetails`

--- LeanBlueprint.lean
+++ LeanBlueprint.lean
@@ -1,3 +1,12 @@
 -- This module serves as the root of the `LeanBlueprint` library.
 -- Import modules here that should be built as part of the library.
 import «LeanBlueprint».Basic
+
+open Lean
+
+def main (args : List String) : IO Unit := do
+  let [inputFile, lakeFile, outputFile] := args | 
+    IO.throwServerError "Supply an input file, lakefile and output file path."
+  let result ← analyzeInput inputFile lakeFile
+  let output := toJson result 
+  IO.FS.writeFile outputFile (toString output)

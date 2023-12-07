089ef7f511259492187bd89192b1dfeff59c23f2
ART0 <18333981+0Art0@users.noreply.github.com>
2023-12-07 11:24:44 +0000

    Testing non-transitivity of `sorry`

--- Test/Test.lean
+++ Test/Test.lean
@@ -9,3 +9,8 @@ def a := false
 def b := -1
 
   theorem flt : ∀ a b c n : Nat, n > 2 → a^n + b^n ≠ c^n := by sorry
+
+theorem flt3 : ∀ a b c : Nat, a^3 + b^3 ≠c^3 := by
+  intro a b c
+  apply flt
+  decide
